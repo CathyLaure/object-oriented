f12523c079dcd35cb55b0f21ef740e42ddeadd4c
Cathy Tasama <ctasama@cnm.edu>
2020-04-13 23:29:54 -0600

    added Test testGetValidAuthorByAuthorId and GetTestValidAuthors methods.

--- php/classes/Test/AuthorTest.php
+++ php/classes/Test/AuthorTest.php
@@ -109,15 +109,53 @@ private $Valid_Username = "ctasama";
 		//verify the record count has returned to the original count before the record was inserted.
 		self::assertEquals($this->getConnection()->getRowCount("author"), $numRows);
 	}
-/*
-	public function testGetValidAuthorByAuthorId() : void {
 
+	public function testGetValidAuthorByAuthorId() : void {
+		//how many records were in the db before we start?
+		$numRows = $this->getConnection()->getRowCount("author");
+
+		//now insert a row of data
+		$authorId = generateUuidV4()->toString();
+		$author = new Author($authorId, $this->Valid_Activation_Token, $this->Valid_Avatar_Url, $this->Valid_Author_Email, $this->Valid_Author_Hash, $this->Valid_Username);
+		$author->insert($this->getPDO());
+
+		//validate new row count in the table - should be old row count + 1 if insert is successful
+		$numRowsAfterInsert = $this->getConnection()->getRowCount("author");
+		self::assertEquals($numRows + 1, $numRowsAfterInsert);
+
+		//now get the row we just inserted and verify that the data coming out of the db matches the data we put in the db
+		$pdoAuthor = Author::getAuthorByAuthorId($this->getPDO(), $author->getAuthorId()->toString());
+		self::assertEquals($pdoAuthor->getAuthorId(), $authorId);
+		self::assertEquals($pdoAuthor->getAuthorActivationToken(), $this->Valid_Activation_Token);
+		self::assertEquals($pdoAuthor->getAuthorAvatarUrl(), $this->Valid_Avatar_Url);
+		self::assertEquals($pdoAuthor->getAuthorEmail(), $this->Valid_Author_Email);
+		self::assertEquals($pdoAuthor->getAuthorHash(), $this->Valid_Author_Hash);
+		self::assertEquals($pdoAuthor->getAuthorUsername(), $this->Valid_Username);
 	}
 
 	public function testGetValidAuthors() : void {
+		//how many records were in the db before we start?
+		$numRows = $this->getConnection()->getRowCount("author");
+		$rowsInserted = 5;
 
+		//now insert 5 rows of data
+		for ($i=0; $i<$rowsInserted; $i++){
+			$authorId = generateUuidV4()->toString();
+			$author = new Author($authorId, $this->Valid_Activation_Token,
+				$this->Valid_Avatar_Url,
+				$this->Valid_Author_Email . $i,
+				$this->Valid_Author_Hash,
+				$this->Valid_Username . $i);
+			$author->insert($this->getPDO());
+		}
+
+		//validate new row count in the table - should be old row count + 1 if insert is successful
+		self::assertEquals($numRows + $rowsInserted, $this->getConnection()->getRowCount("author"));
+
+		//validate number of rows coming back from our function.
+		self::assertEquals($numRows + $rowsInserted, $author->getAllAuthors($this->getPDO())->count());
 	}
-*/
+
 
 }
 
